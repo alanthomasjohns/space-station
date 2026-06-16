95b1b8280ec477ff9f1b5b34ec86817b93ccd4ca
alanthomasjohns <alanthomasjohns99@gmail.com>
2026-06-16 11:43:47 +0530

    refactored UI and added click option

--- frontend/src/components/Globe.tsx
+++ frontend/src/components/Globe.tsx
@@ -1,30 +1,44 @@
 import { useEffect } from "react";
-import { Viewer, Cartesian2 , Cartesian3, Color } from "cesium";
-import { getSatellites } from "../api/satellites";
+import { Viewer, Cartesian2, Cartesian3, Color } from "cesium";
 
+import { getSatellites } from "../api/satellites";
+import type { Satellite } from "../types/satellite";
 
 function Globe() {
-    function flyToLocation(viewer: Viewer, latitude: number,longitude: number,) 
-    {
+    function flyToLocation(
+        viewer: Viewer,
+        latitude: number,
+        longitude: number,
+    ) {
         viewer.camera.flyTo({
             destination: Cartesian3.fromDegrees(
                 longitude,
                 latitude,
-                1500000,
+                1_500_000,
             ),
         });
     }
 
-    function addUserMarker(viewer: Viewer, latitude: number, longitude: number,)
-    {
+    function addUserMarker(
+        viewer: Viewer,
+        latitude: number,
+        longitude: number,
+    ) {
         viewer.entities.add({
+            id: "user",
+
             position: Cartesian3.fromDegrees(
                 longitude,
                 latitude,
                 0,
             ),
 
-            point: {pixelSize: 10, color: Color.DODGERBLUE, outlineColor: Color.WHITE, outlineWidth: 2},
+            point: {
+                pixelSize: 10,
+                color: Color.DODGERBLUE,
+                outlineColor: Color.WHITE,
+                outlineWidth: 2,
+            },
 
             label: {
                 text: "You are here",
@@ -38,31 +52,65 @@ function Globe() {
         });
     }
 
-    function addSatellite(viewer: Viewer, name: string, latitude: number,longitude: number, altitudeKm: number,)
-    {
-        viewer.entities.add(
-            {
-                position: Cartesian3.fromDegrees(
-                longitude,
-                latitude,
-                altitudeKm * 1000
-                ),
-                point: {
-                    pixelSize: 5,
-                    color: Color.RED
-                },
-                label: {
-                    text: name,
-                    pixelOffset: new Cartesian2(0, -10),
-                    fillColor: Color.WHITE,
-                    outlineColor: Color.BLACK,
-                    outlineWidth: 2,
-                    showBackground: true,
-                    backgroundColor: Color.BLACK.withAlpha(0.25),
-                },
-            }
-        );
+    function addOrUpdateSatellite(
+        viewer: Viewer,
+        satellite: Satellite,
+    ) {
+        const entity = viewer.entities.getById(satellite.name);
+
+        if (entity) {
+            entity.position = Cartesian3.fromDegrees(
+                satellite.longitude,
+                satellite.latitude,
+                satellite.altitude_km * 1000,
+            );
+
+            return;
+        }
+
+        viewer.entities.add({
+            id: satellite.name,
+
+            position: Cartesian3.fromDegrees(
+                satellite.longitude,
+                satellite.latitude,
+                satellite.altitude_km * 1000,
+            ),
+
+            point: {
+                pixelSize: 5,
+                color: Color.RED,
+            },
+
+            label: {
+                text: satellite.name,
+                pixelOffset: new Cartesian2(0, -10),
+                fillColor: Color.WHITE,
+                outlineColor: Color.BLACK,
+                outlineWidth: 2,
+                showBackground: true,
+                backgroundColor: Color.BLACK.withAlpha(0.25),
+            },
+
+            properties: {
+                altitudeKm: satellite.altitude_km,
+            },
+
+            description: `
+                <h3>${satellite.name}</h3>
+                <p>Altitude: ${satellite.altitude_km.toFixed(1)} km</p>
+            `,
+        });
     }
+
+    async function refreshSatellites(viewer: Viewer) {
+        const satellites = await getSatellites();
+
+        for (const satellite of satellites) {
+            addOrUpdateSatellite(viewer, satellite);
+        }
+    }
+
     useEffect(() => {
         const viewer = new Viewer("cesium-container", {
             animation: false,
@@ -74,26 +122,49 @@ function Globe() {
             sceneModePicker: false,
             fullscreenButton: false,
         });
-        navigator.geolocation.getCurrentPosition(async(position) => {
 
+        viewer.selectedEntityChanged.addEventListener((entity) => {
+            if (!entity) {
+                return;
+            }
+
+            console.log(entity.id);
+        });
+
+        let intervalId: number;
+
+        navigator.geolocation.getCurrentPosition(async (position) => {
             const latitude = position.coords.latitude;
             const longitude = position.coords.longitude;
 
-            flyToLocation(viewer, latitude, longitude,);
-            addUserMarker(viewer, latitude, longitude,);
-            const satellites = await getSatellites();
-            for (const satellite of satellites) {
-                addSatellite(
-                    viewer,
-                    satellite.name,
-                    satellite.latitude,
-                    satellite.longitude,
-                    satellite.altitude_km,
-                );
-            }
+            flyToLocation(viewer, latitude, longitude);
+            addUserMarker(viewer, latitude, longitude);
+
+            await refreshSatellites(viewer);
+
+            intervalId = window.setInterval(() => {
+                refreshSatellites(viewer);
+            }, 5000);
         });
+
+        return () => {
+            if (intervalId) {
+                window.clearInterval(intervalId);
+            }
+
+            viewer.destroy();
+        };
     }, []);
-    return (<div id="cesium-container" style={{ width: "100%", height: "600px" }} />);
+
+    return (
+        <div
+            id="cesium-container"
+            style={{
+                width: "100%",
+                height: "600px",
+            }}
+        />
+    );
 }
 
 export default Globe;
